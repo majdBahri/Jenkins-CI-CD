a0625866861880261e9bb5c3f37e56f876532254
majdBahri <majd.bahri19@gmail.com>
2026-01-08 12:16:51 +0000

    Fix formatting in test_app.py

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ class TestApp(unittest.TestCase):
     def setUp(self):
         self.client = app.test_client()
         self.client.testing = True
-
+ 
     def test_home_page(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
